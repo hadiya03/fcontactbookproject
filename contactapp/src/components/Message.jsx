51101c5c49e31d41e1d74e8b019948fbee300f51
hadiya03 <hadiyamk03@gmail.com>
2025-07-09 09:36:40 +0530

    Update Message.jsx

--- contactapp/src/components/Message.jsx
+++ contactapp/src/components/Message.jsx
@@ -19,7 +19,7 @@ const Message = () => {
   const entry = location.state;
 
   const handleBack = () => {
-    navigate('/br');
+    navigate('/birthdays');
   };
 
   const name = entry?.name || 'Friend';
